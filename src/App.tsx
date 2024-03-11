dbd43fc03af4822210aedc8a2c4db186fd2a857a
kimpors <mikita_korshak@ukr.net>
2024-03-11 18:42:13 +0200

    Add state to tabs
    now i can move between tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import Card from './components/Card.tsx'
 import Option from './components/Option.tsx'
 import Matrix from './components/Matrix.tsx'
+import { useState } from 'react';
 
 export default function App() {
+	let [moreA, setMoreA] = useState(false);
+	let [moreB, setMoreB] = useState(false);
+	let [matrix, setMatrix] = useState(true);
+
+
+
+
 	return (
 		<article className="main">
 			<article className='container'>
 				<section className='tab'>
-					<input className='top-radius shadow' type="button" value="=" />
+					<input className='top-radius shadow' type="button" value="="  onClick={ () => setMoreA(!moreA) }/>
 				</section>
-				<Card>
-					<Matrix size={4}/>
-				</Card>
+				{moreA 
+					? <Card><h1>More</h1></Card>
+					: <Card><Matrix size={4}/></Card> 
+				}
 			</article>
 		
 			<Option className='flex-col p-5'>
@@ -22,19 +31,27 @@ export default function App() {
 			</Option>
 			<article className='container'>
 				<section className='tab'>
-					<input className='top-radius shadow' type="button" value="=" />
-					<input className='top-radius shadow' type="button" value="M" />
-					<input className='top-radius shadow' type="button" value="N" />
+					<input className='top-radius shadow' type="button" value="=" onClick={ () => setMoreB(!moreB) } />
+					<input className='top-radius shadow' type="button" value="M" onClick={ () => setMatrix(true) } />
+					<input className='top-radius shadow' type="button" value="N" onClick={ () => setMatrix(false) }/>
 				</section>
-			
-				<Card>
-					<Matrix size={4}/>
-					<Option className='flex-row mx-2 p-2'>
-						<button type="button">+</button>
-						<button type="button">clean</button>
-						<button type="button">-</button>
-					</Option>
-				</Card>
+
+				{moreB
+					? <Card><h1>More</h1></Card>
+					: (
+						<Card>
+							{matrix
+								? <Matrix size={4}/>
+								: <input type="number" value="hello" />
+							}
+							<Option className='flex-row mx-2 p-2'>
+								<button type="button">+</button>
+								<button type="button">clean</button>
+								<button type="button">-</button>
+							</Option>
+						</Card>
+					)
+				}
 			</article>
 		</article>
 	)
