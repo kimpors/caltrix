4bc2d6ccdbb5e5938b77b3bc12a02ff50d20eafa
kimpors <mikita_korshak@ukr.net>
2024-04-05 06:59:05 +0300

    Set theme in initilize component

--- src/app/components/Initilize.tsx
+++ src/app/components/Initilize.tsx
@@ -2,14 +2,18 @@
 
 import { useEffect } from "react";
 import { useStateContext } from "./StateContext";
+import { useTheme } from "next-themes";
 
 export default function Initilize() {
+	const { setTheme } = useTheme();
 	const { setLeft, setRight } = useStateContext();
+
 
 	useEffect(() => {
 		if (!localStorage.getItem("left")) {
 			localStorage.setItem("left", JSON.stringify(Array<Array<number>>(3).fill(Array(3).fill(0))));
 			localStorage.setItem("right", JSON.stringify(Array<Array<number>>(3).fill(Array(3).fill(0))));
+			setTheme("pink");
 		}
 
 		setLeft(JSON.parse(localStorage.getItem("left") || "[]") as number[][])
